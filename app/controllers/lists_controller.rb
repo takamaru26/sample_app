a0222928b62b4de7c80ce8869e9f63cd4a1bf0ad
EC2 Default User <ec2-user@ip-172-31-38-245.ap-northeast-1.compute.internal>
2023-08-10 05:19:23 +0000

    6章まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,20 +9,30 @@ def create
   list = List.new(list_params)
   # 3. データをデータベースに保存するためのsaveメソッド実行
   list.save
-  # 4. トップ画面へリダイレクト
-  redirect_to '/top'
+  
+  redirect_to list_path(list.id)
 end
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id]) 
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+    
+  private
     # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
